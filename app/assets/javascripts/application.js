fb4e53c6324699a320127e09505a8342f4417513
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-12-14 16:44:16 -0600

    Add and install foundation-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require underscore
 //= require backbone
 //= require web_mlr
@@ -21,3 +22,5 @@
 //= require_tree ./views
 //= require_tree ./routers
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
