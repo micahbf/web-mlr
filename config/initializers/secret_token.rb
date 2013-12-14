905979abbf4229507afb7c34ef0c5e90eefaefe3
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-12-14 15:45:43 -0600

    Initial Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebMlr::Application.config.secret_key_base = '1d61f60395bea1bde702958671020d40aeed72082cfd88f0719d91d6c32263f7f915fcb88a36b7bb8d96a1cf658242f700d90d7443bbb730cee9a6c8c314af95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebMlr::Application.config.session_store :cookie_store, key: '_web-mlr_session'
